ae885eb9131a96f11cfbbd26f5abd26a772be878
Ian Ingram <ian@ingramclockworks.com>
2014-06-09 15:52:20 -0700

    Perl script for cleaning/prepping the output from Blender for use in my gestureController code.

---
+++ clean.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $last1 = 1000000000000000000;
+my $last2 = 42;
+my $divisor = 46001413368/1000;
+my $fixed = 0;
+
+while(<STDIN>){
+#    print "$1,$2\n" if /^\s{7}(\d*),([-+]?[0-9]*\.[0-9]+),/;
+    
+    if(/^\s{7}(\d*),([-+]?[0-9]*\.[0-9]+),/){
+# this will cut off the very last entry but I am going to consider that fine for now
+	$fixed = $last1/$divisor;
+	$fixed = int($fixed);
+ 	print "$fixed,$last2\n" if $last1 < $1;
+ 	
+ 	$last1 = $1;
+ 	$last2 = $2;
+    }
+}
