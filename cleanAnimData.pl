658377113099aec550d1f5b79452e7c18cbde765
Reid Mitchell <rbm02012@mymail.pomona.edu>
2014-06-28 14:28:25 -0400

    Updated cleanAnimData to take command-line args

--- cleanAnimData.pl
+++ cleanAnimData.pl
@@ -2,23 +2,61 @@
 use strict;
 use warnings;
 
-my $last1 = 1000000000000000000;
-my $last2 = 42;
 my $divisor = 46001413368/1000;
 my $fixed = 0;
+
+my $queryModel = $ARGV[0];        #in this case: Cube, Camera, or Lamp
+my $queryChannel = "Transform";   #not sure what other possibilities are - only Transform appears in .fbx
+my $queryAnimType = $ARGV[1];     #(T)ranslate, (R)otate, (S)cale
+my $queryAxis = $ARGV[2];         #X, Y, Z
+my $offset = $ARGV[3];            #offset added to correct values (use 90 in this case, 0 if no offset needed)
+
+my $modelCheck = 0;
+my $channelCheck = 0;
+my $animTypeCheck = 0;
+my $axisCheck = 0;
 
 print "time,value\n";
 
 while(<STDIN>){
-#    print "$1,$2\n" if /^\s{7}(\d*),([-+]?[0-9]*\.[0-9]+),/;
-    
-    if(/^\s{7}(\d*),([-+]?[0-9]*\.[0-9]+),/){
-# this will cut off the very last entry but I am going to consider that fine for now
-	$fixed = $last1/$divisor;
-	$fixed = int($fixed);
- 	print "$fixed,$last2\n" if $last1 < $1;
- 	
- 	$last1 = $1;
- 	$last2 = $2+90;  #this adjusts for an offset that comes out of blender (at least on the rotation I am using)
+
+  if(/^\s{2}Model: "Model::(.*)"/) {
+    if($1 eq $queryModel) {
+      $modelCheck = 1;
+    } else {
+      $modelCheck = 0;
     }
+  }
+
+  if(/^\s{3}Channel: "(.*)"/) {
+    if($1 eq $queryChannel) {
+      $channelCheck = 1;
+    } else {
+      $channelCheck = 0;
+    }
+  }
+  elsif(/^\s{4}Channel: "(.*)"/) {
+    if($1 eq $queryAnimType) {
+      $animTypeCheck = 1;
+    } else {
+      $animTypeCheck = 0;
+    }
+  }
+  elsif(/^\s{5}Channel: "(.*)"/) {
+    if($1 eq $queryAxis) {
+      $axisCheck = 1;
+    } else {
+      $axisCheck = 0;
+    }
+  }
+
+    
+  if(/^\s{7}(\d*),([-+]?[0-9]*\.[0-9]+),/){
+	$fixed = $1/$divisor;
+	$fixed = int($fixed);
+	my $corrected = $2 + $offset;
+	if ($channelCheck && $modelCheck && $animTypeCheck && $axisCheck) {
+	  print "$fixed,$corrected\n";
+	}
+  }
 }
